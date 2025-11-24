eacedb72504384550e5b55ee08b74edec93b98eb
tobynorth <toby.ross.north@gmail.com>
2025-11-24 18:24:24 -0500

    Use const for numPlayers instead of magic number

--- src/App.ts
+++ src/App.ts
@@ -2,6 +2,8 @@ import { Client } from 'boardgame.io/client';
 import { StarSystemCard, ItineraryCard, TokenEffectsConfig, makeLightsInTheVoidGame } from './Game';
 import { CardLoader } from './data/CardLoader';
 
+const NUM_PLAYERS = 2;
+
 class LightsInTheVoidClient {
   client: ReturnType<typeof Client>;
   constructor(
@@ -10,14 +12,14 @@ class LightsInTheVoidClient {
     tokenEffectsConfig: TokenEffectsConfig
   ) {
     const litv = makeLightsInTheVoidGame(starSystemCards, itineraryCards, tokenEffectsConfig);
-    this.client = Client({ game: litv, numPlayers: 2 });
+    this.client = Client({ game: litv, numPlayers: NUM_PLAYERS });
     this.client.start();
   }
 }
 
 Promise.all([
   CardLoader.loadStarSystemCards(),
-  CardLoader.loadItineraryCards(2),
+  CardLoader.loadItineraryCards(NUM_PLAYERS),
   CardLoader.loadTokenEffects()
 ]).then(([starSystemCards, itineraryCards, tokenEffectsConfig]) => {
   const app = new LightsInTheVoidClient(starSystemCards, itineraryCards, tokenEffectsConfig);
